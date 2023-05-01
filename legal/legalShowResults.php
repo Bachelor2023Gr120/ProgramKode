cd24210ad938ef8ba0ecca8bd2d4955f76d127f8
JonasFSi <Jonasfsi@stud.ntnu.no>
2023-05-01 12:40:42 +0200

    b

--- legal/legalShowResults.php
+++ legal/legalShowResults.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="legal.css">
 
+
+    
     <link rel="stylesheet" href="displayResultStyleCopy.css">
 
 
