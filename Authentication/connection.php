320bd446c7550ed7bdb32da6d84cd6cbbc916bfb
JonasFSi <Jonasfsi@stud.ntnu.no>
2023-05-02 10:08:11 +0200

    fix connection to database machine

--- Authentication/connection.php
+++ Authentication/connection.php
@@ -1,8 +1,8 @@
 <?php      
         // Variables used in the database connection
-    $host = "localhost";  //Database serveren
+    $host = "192.168.1.25";  //Database serveren
     $email = "root";  
-    $password = '';  
+    $password = 'passord';  
     $db_name = "usercompanydb";  
                     // Establishing the connection to the MySql/DB
     $con = mysqli_connect($host, $email, $password, $db_name);  
